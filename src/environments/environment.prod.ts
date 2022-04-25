c4d3c2e7c1041ca44d3bec6b7bdd40ce505c411c
golukumar21 <golukumar21@gmail.com>
2022-04-25 10:48:53 +0530

    Auto-generated commit [ci skip]

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-const HOST = 'https://api.github.com/users/johnpapa';
-
-export const environment = {
-  production: true,
-  FYLE_GIT: {
-    userDetails: HOST,
-    repos: HOST + '/repos',
-  },
-};
